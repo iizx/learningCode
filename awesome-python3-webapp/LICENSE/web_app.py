a42a4ac381eb2abadac76b8fa297d52dc24f1adc
my git <610942188@qq.com>
2019-03-13 22:49:49 +0800

    update

--- awesome-python3-webapp/LICENSE/web_app.py
+++ awesome-python3-webapp/LICENSE/web_app.py
@@ -26,3 +26,55 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 # 持续运行
 loop.run_forever()
+
+# 创建一个连接池
+import aiomysql
+@asyncio.coroutine
+def create_pool(loop,**kw):
+    logging.info('start creats databases connection pool...')
+    # 定义一个全局参数
+    global _pool
+    # 使用kw传入的参数创建一个连接池，方便复用
+    _pool = yield from aiomysql.create(
+        host = kw.get('host','localhost'),
+        port = kw.get('port','3320'),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf8'),
+        autocommit = kw.get('autocommit',True),
+        maxsize = kw.get('maxsize','10'),
+        minsize = kw.get('minsize','1'),
+        loop = loop
+    )
+
+# 封装select函数
+@asyncio.coroutine
+def select(sql,args,size = None):
+    logging.info(sql,args)
+    global _pool
+    with (yield from _pool) as cnn:
+        # 创建协程游标
+        cur  = yield from cnn.cursor(aiomysql.DictCursor) #查询的返回格式会变成字典格式
+        yield from cur.execute(sql.replace('?',"%s"), args or ())#替换为sql中的占位符
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+            # 如果传入size参数，就通过fetchmany()获取最多指定数量的记录，否则，通过fetchall()获取所有记录。
+        yield from cur.close()
+        logging.info('rows returned:%s' % len(rs))
+        return rs
+# 封装insert/update/delete函数
+@asyncio.coroutine
+def execute(sql,args):
+    logging.info(sql)
+    with (yield from _pool) as cnn:
+        try:
+            cur = yield from cnn.cursor()
+            yield from cur.execute(sql.replace('?','%s'),args)
+            affected  = cur.rowcount
+            yield from cur.closed()
+        except BaseException as e:
+            raise
+        return affected
